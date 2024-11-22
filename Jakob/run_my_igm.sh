c416b19bbfc3c4f38dfdd16a1ae3d2308d29de2b
Jakob Gradl <jakob.gradl@utas.edu.au>
2024-11-22 14:34:23 +1100

    run_my_igm

--- Jakob/run_my_igm.sh
+++ Jakob/run_my_igm.sh
@@ -2,19 +2,20 @@ for c_row in $(seq 1 3); do
 
     c_fname=$(awk 'NR=='$c_row' {print $1}' my_igm_params.dat)
     # cparam=(awk 'NR==$crow, {print, $2}' my_igm_params.txt)
-    c_value=$(awk 'NR=='$c_row' {print $3}' my_igm_params.dat)
+    c_value=$(awk 'NR=='$c_row' {print $2}' my_igm_params.dat)
 
-    for a_row in $(seq 4 6); do
+    for a_row in $(seq 5 7); do
 
         a_fname=$(awk 'NR=='$a_row' {print $1}' my_igm_params.dat)
         # aparam=(awk 'NR==$crow, {print, $2}' my_igm_params.txt)
-        a_value=$(awk 'NR=='$a_row' {print $3}' my_igm_params.dat)
+        a_value=$(awk 'NR=='$a_row' {print $2}' my_igm_params.dat)
 
         replaceparam="output/synthetic_forward"$c_fname$a_fname".nc"
         echo $replaceparam
 
         # sed -i '/iflo.save-file/c $replaceparam' /temp/foo
-        igm_run --iflo_init_slidingco $c_value --iflo_init_arrhenius $a_value --wncd_output_file $replaceparam | tee -a output/synthetic_forward_linSurf_igm_output.txt
+        igm_run --iflo_init_slidingco $c_value --iflo_init_arrhenius $a_value --wncd_output_file $replaceparam 
+        #| tee -a output/synthetic_forward_linSurf_igm_output.txt
         wait
 
     done
