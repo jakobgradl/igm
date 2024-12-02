48f26c465307a796e3d19ab7624b9d5085fc82dd
Jakob Gradl <jakob.gradl@utas.edu.au>
2024-12-03 10:18:36 +1100

    igm runner shell script

--- Jakob/run_my_igm.sh
+++ Jakob/run_my_igm.sh
@@ -1,16 +1,16 @@
-for c_row in $(seq 1 3); do
+for c_row in $(seq 2 2); do
 
     c_fname=$(awk 'NR=='$c_row' {print $1}' my_igm_params.dat)
     # cparam=(awk 'NR==$crow, {print, $2}' my_igm_params.txt)
     c_value=$(awk 'NR=='$c_row' {print $2}' my_igm_params.dat)
 
-    for a_row in $(seq 5 7); do
+    for a_row in $(seq 6 6); do
 
         a_fname=$(awk 'NR=='$a_row' {print $1}' my_igm_params.dat)
         # aparam=(awk 'NR==$crow, {print, $2}' my_igm_params.txt)
         a_value=$(awk 'NR=='$a_row' {print $2}' my_igm_params.dat)
 
-        replaceparam="output/synthetic_forward"$c_fname$a_fname".nc"
+        replaceparam="output/synthetic_forward_logSurf"$c_fname$a_fname".nc"
         echo $replaceparam
 
         # sed -i '/iflo.save-file/c $replaceparam' /temp/foo
